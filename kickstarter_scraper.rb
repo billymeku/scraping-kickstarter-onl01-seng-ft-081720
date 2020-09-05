1c102091e200eae56852b3cfb7ac72e4af8f4ad9
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-05 03:38:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,5 +5,9 @@ require "pry"
  
 def create_project_hash
   # write your code here
-
+html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+ 
+create_project_hash
